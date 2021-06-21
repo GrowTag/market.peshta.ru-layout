b28ca4b18aa07e4a83b956ab248a58ce86e67962
maxim <winscode@mail.ru>
2021-06-21 13:33:42 +0400

    пешка 2

---
+++ peshta2/dest/scripts/script.js
@@ -0,0 +1,41 @@
+
+
+if($('.main-slider').length !== 0){
+    $('.main-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        fade: true,
+        arrows: true,
+        nextArrow: '<span class="arrow-next"></span>',
+        prevArrow: '<span class="arrow-prev"></span>',
+        asNavFor: '.thumb-slider'
+    });
+
+    $('.thumb-slider').slick({
+        infinite: true,
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        arrows: false,
+        focusOnSelect: true,
+        asNavFor: '.main-slider',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 4
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 2
+                }
+            }
+        ]
+    });
+}
+
+
+
